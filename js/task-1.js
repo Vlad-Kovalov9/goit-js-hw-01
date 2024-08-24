e98e649491464f489da9eeda60646dd19f7748e8
Vlad <nowornever969@gmail.com>
2024-08-24 09:53:15 +0300

    add

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,9 @@
-const userName = "Alice";
-const text = "Hello";
+'use strict';
 
-const total = userName + text;
+function makeTransaction(quantity, pricePerDroid) {
+    return `You ordered ${quantity} droids worth ${pricePerDroid} credits!`;
+}
 
-console.log(total);
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
